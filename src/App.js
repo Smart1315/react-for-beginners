4311cfb9ef90e250492f64b0ea1bb41598704217
pro <pro@wordncode.com>
2023-12-29 18:46:42 +0900

    Movie App

--- src/App.js
+++ src/App.js
@@ -1,49 +1,36 @@
-import {useEffect, useState} from "react";
+/*
+* react-router-dom 5버전 -> 버전6 바뀐 부분
+*
+* 1. Switch컴포넌트가 Routes컴포넌트로 대체되었습니다.
+* Switch -> Routes
+*
+* 2. Route컴포넌트 사이에 자식 컴포넌트를 넣지 않고, element prop에 자식 컴포넌트를 할당하도록 바뀌었습니다.
+* Route path="/" element={< Home / >}
+*
+* react-router-dom 6버전 문서
+* https://reactrouter.com/docs/en/v6/getting-started/overview
+* */
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
-    const [amount, setAmount] = useState(0);
-    const [selectedCoin, setSelectedCoin] = useState(null);
-    useEffect(() => {
-        fetch('https://api.coinpaprika.com/v1/tickers')
-            .then((response) => response.json())
-            .then((json) => {
-                setCoins(json);
-                setSelectedCoin(json[0]);
-                setLoading(false);
-            });
-    }, []);
-
-    const onChange = (event) => {
-        setAmount(event.target.value);
-    };
-
-    const handleSelectChange = (event) => {
-        const coinId = event.target.value;
-        setSelectedCoin(coins.find(coin => coin.id === coinId));
-    };
-
     return (
-        <div>
-            <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
-            {loading ? (
-                <strong>Loading...</strong>
-            ) : (
-                <>
-                    <input onChange={onChange} value={amount} type={'number'}/>$
-                    / {selectedCoin ? amount / selectedCoin.quotes.USD.price + ` (${selectedCoin.symbol})` : null}
-                    <br/>
-                    <select onChange={handleSelectChange}>
-                        {coins.map((coin) => (
-                            <option key={coin.id} value={coin.id}>
-                                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-                            </option>
-                        ))}
-                    </select>
-                </>
-            )}
-        </div>
+        <Router>
+            <Routes>
+                <Route path={'/hello'} element={
+                    <h1>Hello</h1>
+                }/>
+                <Route path={'/movie/:id'} element={<Detail/>}/>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/home'} element={<Navigate to={'/'}/>}/>
+            </Routes>
+        </Router>
     );
 }
 
